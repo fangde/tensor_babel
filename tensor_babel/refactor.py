cc745ffe7653a66a1d07227d37ba9dfd151c2268
Fangde Liu <hibana1983@gmail.com>
2018-10-05 20:56:00 +0800

    fixed code compile

--- tensor_babel/refactor.py
+++ tensor_babel/refactor.py
@@ -11,12 +11,14 @@ def refactorFunc(node):
     left_node = node.args[0]
     node.args = node.args[1:-1]
 
-    print node.keywords
+    
     kwl = []
     for kw in node.keywords:
         if kw.arg in kws:
             kwl.append(kw)
+            print "keywords args",kw.arg
         node.keywords.remove(kw)
+        
 
     newNode = ast.Call(
         func=node, args=[left_node], keywords=kwl, starargs=[], kwargs=[])
@@ -27,19 +29,24 @@ def refactorFunc(node):
 class LayerCallRefactor(ast.NodeTransformer):
     def visit_Call(self, node):
         if isinstance(node.func, ast.Attribute):
-            print 'ATTR', node.func.attr
+            
             if node.func.attr in tl_layers:
+                print 'ATTR', node.func.attr,(node.lineno,node.col_offset)
                 return refactorFunc(node)
+            self.generic_visit(node)
 
         if isinstance(node.func, ast.Name):
-            print 'FUNCName', node.func.id
+            
             if node.func.id in tl_layers:
+                print 'FUNCName', node.func.id,(node.lineno,node.col_offset)
                 return refactorFunc(node)
+            self.generic_visit(node)
 
         return node
 
     def visit_Attribute(self, node):
         if node.attr in attr_rename.keys():
+            print "rename",node.attr, (node.lineno,node.col_offset)
             node.attr = attr_rename[node.attr]
 
         return node
