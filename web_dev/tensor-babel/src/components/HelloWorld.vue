8d01fb6c34ce0769680527cb6ab0579c562681c9
Fangde Liu <hibana1983@gmail.com>
2018-10-08 22:28:11 +0800

    add text area

--- web_dev/tensor-babel/src/components/HelloWorld.vue
+++ web_dev/tensor-babel/src/components/HelloWorld.vue
@@ -6,8 +6,17 @@
   <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
 </el-row>
 <el-row>
-  <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+  <el-col :span="12"><div class="grid-content bg-purple">
+     <textarea :value="input" ></textarea>
+  
+    
+    </div></el-col>
+  <el-col :span="12"><div class="grid-content bg-purple-light">
+    
+    <div >
+      compiled code 
+    </div>  
+  </div></el-col>
 </el-row>
   
     
